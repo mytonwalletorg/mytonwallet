41f45bc8d79b0322b5dce6aa4da7199374018a85
Arterialist <37044735+arterialist@users.noreply.github.com>
2023-06-01 15:10:56 +0400

    fix(constants): change jetton forward amount to 1 nanoton

--- src/api/blockchains/ton/constants.ts
+++ src/api/blockchains/ton/constants.ts
@@ -1,5 +1,5 @@
 export const TOKEN_TRANSFER_TON_AMOUNT = '0.05';
-export const TOKEN_TRANSFER_TON_FORWARD_AMOUNT = '0';
+export const TOKEN_TRANSFER_TON_FORWARD_AMOUNT = '0.000000001';
 
 export enum JettonOpCode {
   transfer = 0xf8a7ea5,
