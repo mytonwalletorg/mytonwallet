532c95cda4fae937d5cfc3cfc9d16bddf3743a21
qqqdendi <qqqdendi@gmail.com>
2023-07-20 04:26:39 +0600

    fixed animation

--- src/components/main/modals/BackupModal.tsx
+++ src/components/main/modals/BackupModal.tsx
@@ -169,19 +169,17 @@ function BackupModal({
     <>
       {isInsideModal
         ? (
-          <div className={styles.wrapper}>
-            <Transition
-              name="pushSlide"
-              className={buildClassName(modalStyles.transition, 'custom-scroll')}
-              slideClassName={modalStyles.transitionSlide}
-              activeKey={currentSlide}
-              nextKey={nextKey}
-            >
+          <Transition
+            name="pushSlide"
+            className={buildClassName(modalStyles.transition, 'custom-scroll')}
+            slideClassName={modalStyles.transitionSlide}
+            activeKey={currentSlide}
+            nextKey={nextKey}
+          >
 
-              {renderContent}
+            {renderContent}
 
-            </Transition>
-          </div>
+          </Transition>
         ) : (
           <Modal
             hasCloseButton
