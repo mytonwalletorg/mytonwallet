c62be3bd12946be8ec0e491ce12640a1e2ed5766
mytonwalletorg <mytonwallet@gmail.com>
2024-04-25 03:55:18 +0400

    v1.19.14

--- src/util/openUrl.ts
+++ src/util/openUrl.ts
@@ -2,21 +2,22 @@ import { AppLauncher } from '@capacitor/app-launcher';
 import { getActions } from '../global';
 
 import { IS_CAPACITOR } from '../config';
-import safeExec from './safeExec';
 
 export async function openUrl(url: string, isExternal?: boolean) {
   if (IS_CAPACITOR && !isExternal && url.startsWith('http')) {
     getActions().openBrowser({ url });
-  } else if (IS_CAPACITOR && await canOpenApp(url)) {
-    AppLauncher.openUrl({ url });
   } else {
-    window.open(url, '_blank', 'noopener');
+    const couldOpenApp = IS_CAPACITOR && await openAppSafe(url);
+    if (!couldOpenApp) {
+      window.open(url, '_blank', 'noopener');
+    }
   }
 }
 
-function canOpenApp(url: string) {
-  return safeExec(async () => {
-    const result = await AppLauncher.canOpenUrl({ url });
-    return result.value;
-  });
+async function openAppSafe(url: string) {
+  try {
+    return (await AppLauncher.openUrl({ url })).completed;
+  } catch (err) {
+    return false;
+  }
 }
