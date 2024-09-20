96232b5b48eeda14ac4d921fb9d8411c8de696db
mytonwalletorg <mytonwallet@gmail.com>
2024-09-20 12:28:47 +0200

    v3.0.15

--- src/config.ts
+++ src/config.ts
@@ -338,6 +338,7 @@ export const DIESEL_TOKENS = new Set([
   'EQAvlWFDxGF2lXm67y4yzC17wYKD9A0guwPkMs1gOsM__NOT', // NOT
   'EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs', // USDT
   'EQCvxJy4eG8hyHBFsZ7eePxrRsUQSFE_jpptRAYBmcG_DOGS', // DOGS
+  'EQD-cvR0Nz6XAyRBvbhz-abTrRC6sI5tvHvvpeQraV9UAAD7', // CATI
 ]);
 
 export const TINY_TOKENS = DIESEL_TOKENS;
