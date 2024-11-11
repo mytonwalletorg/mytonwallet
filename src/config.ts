35b3839aa7a53f59a0dd16194600528fb77bdaa4
b2nkuu <ban.kuu@yahoo.com>
2024-11-11 23:49:09 +0700

    [Сurrency] add THB currency

--- src/config.ts
+++ src/config.ts
@@ -264,6 +264,7 @@ export const SHORT_CURRENCY_SYMBOL_MAP = {
   EUR: '€',
   RUB: '₽',
   CNY: '¥',
+  THB: '฿',
 };
 export const CURRENCY_LIST: { value: ApiBaseCurrency; name: string }[] = [
   {
@@ -278,6 +279,9 @@ export const CURRENCY_LIST: { value: ApiBaseCurrency; name: string }[] = [
   }, {
     value: 'CNY',
     name: 'Yuan',
+  }, {
+    value: 'THB',
+    name: 'Baht',
   }, {
     value: 'BTC',
     name: 'Bitcoin',
